f1a3c7fba800253d813105ca820c0ce0e8ddd6d2
yassine <yassinemarbouh@yahoo.fr>
2021-12-29 20:28:53 +0100

    pays

---
+++ PAYS/spain.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="./countrystyle.css">
+    <link rel="stylesheet" href="../navbarcss.css">
+    <link rel="stylesheet" href="../footercss.css">
+</head>
+<body>
+<?php include('../NavBar.php'); ?>
+<?php include('../Footer.php'); ?>
+</body>
