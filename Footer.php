5057009774552372d6aba7d9bb4771a7bab0ce06
Yassine <yassinemarbouh@yahoo.fr>
2022-03-08 13:03:16 +0100

    links

--- Footer.php
+++ Footer.php
@@ -1,10 +1,10 @@
 
 <div class="footer">
     <div class="social">
-        <i class="bi bi-facebook facebook"></i>
-        <i class="bi bi-twitter twitter"></i>
-        <i class="bi bi-pinterest pinterest"></i>
-        <i class="bi bi-instagram instagram"></i>
+        <a href="https://www.facebook.com/"><i class="bi bi-facebook facebook"></i></a>
+        <a href="https://twitter.com/"><i class="bi bi-twitter twitter"></i></a>
+        <a href="https://www.pinterest.fr/"><i class="bi bi-pinterest pinterest"></i></a>
+        <a href="https://www.instagram.com/"><i class="bi bi-instagram instagram "></i></a>
     </div>
     <p class="copyright">© Worldfood 2021 - Tous droits réservés</p>
 </div>
